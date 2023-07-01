bbddc9c7b6b25d5d69f70abb93ac14a89b95a02e
Zahin Fuad <zahinfuad3i@gmail.com>
2023-07-01 20:13:57 +0600

    Add Logic for a Single Game

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ let getComputerChoice = () => {
     let computerChoice; // Stores the computer's choice (Rock, Paper, or Scissors)
     switch (randomNumber) {
         case 0:
-            computerChoice = "Rock";
+            computerChoice = "rock";
             break;
         case 1: 
-            computerChoice = "Paper";
+            computerChoice = "paper";
             break;
         case 2: 
-            computerChoice = "Scissors";
+            computerChoice = "scissors";
             break;
         default:
             computerChoice = "Oops, Something's Wrong!";
@@ -21,4 +21,50 @@ let getComputerChoice = () => {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+// Play a Single Game of Rock, Paper, or Scissors Between Player and Computer
+let singleGame = (playerSelection, computerSelection) => {
+    // If Player Selects Rock
+    if ((playerSelection.toLowerCase() === "rock") && (computerSelection === "rock")) {
+        return "Draw";
+    }
+    else if ((playerSelection.toLowerCase() === "rock") && (computerSelection === "scissors")) {
+        return "You Win!";
+    }
+    else if ((playerSelection.toLowerCase() === "rock") && (computerSelection === "paper")) {
+        return "You Lose!";
+    }
+
+    // If Player Selects Paper
+    else if ((playerSelection.toLowerCase() === "paper") && (computerSelection === "paper")) {
+        return "Draw";
+    }
+    else if ((playerSelection.toLowerCase() === "paper") && (computerSelection === "rock")) {
+        return "You Win!";
+    }
+    else if ((playerSelection.toLowerCase() === "paper") && (computerSelection === "scissors")) {
+        return "You Win!";
+    }
+
+    // If Player Selects Scissors
+    else if ((playerSelection.toLowerCase() === "scissors") && (computerSelection === "scissors")) {
+        return "Draw";
+    }
+    else if ((playerSelection.toLowerCase() === "scissors") && (computerSelection === "rock")) {
+        return "You Lose!";
+    }
+    else if ((playerSelection.toLowerCase() === "scissors") && (computerSelection === "paper")) {
+        return "You Win!";
+    }
+
+    // Scissors Beats Paper
+    // Paper Beats Rock
+    // If playerSelection is equal to computerChoice Then Draw
+}
+
+let playerSelection = prompt("Please Enter Rock, Paper, or Scissors"); // Ask Player to Enter their Choice
+let computerSelection = getComputerChoice(); // Get the Computer's Selection from the computerChoice function
+
+console.log(`Player: ${playerSelection}, Computer: ${computerSelection}`)
+console.log(singleGame(playerSelection, computerSelection)); // Play a Single Game
